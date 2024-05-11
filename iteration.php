bdd16b03431ca80b5bf1acd50ffd8c9933d2391e
tcccc <122869184+tc10300@users.noreply.github.com>
2024-05-11 22:03:15 +0800

    Create iteration.php

---
+++ iteration.php
@@ -0,0 +1,11 @@
+class Solution {
+    function getSum($a, $b) {
+        while ($b != 0) {
+            $ret = ($a & $b) << 1;
+            $a = $a ^ $b;
+            $b = $ret;
+        }
+        return $a;
+    }
+}
+}
